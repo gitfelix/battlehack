04d4d82f3bd20717ffd2fdd08d06e5efae3948a1
gitfelix <felixmarkusweber@yahoo.it>
2014-06-21 23:04:06 +0200

    adde mailer, bunch of other random stuff

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,3 +1,14 @@
 class Thing < ActiveRecord::Base
    mount_uploader :image, ImageUploader
+   after_create :check_for_bounties
+
+  def check_for_bounties
+    Bounty.where(name: name).each do |bounty|
+
+      ActionMailer::Base.mail(:from => 'from@domain.com', :to => bounty.email,  :subject => "Your Item was Found!", :body => bounty.name).deliver
+
+    end
+  end
+
+
 end
