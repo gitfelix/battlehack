6c8c599115548c2b6b88df7730a3a70fb3670529
speedracr <jonas@robocomobo.com>
2014-06-22 00:45:58 +0200

    Update JS

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,34 +1,51 @@
 function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(53, 13),
-      zoom: 7
+      zoom: 13
     };
-     map = new google.maps.Map(document.getElementById("map-canvas"),
+    
+    map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
-
-     things.forEach(function(thing){
-      console.log(thing.lat)
-      console.log(thing.lng)
+    
+    things.forEach(function(thing){
       position = new google.maps.LatLng(thing.lat,thing.lng)
-      add_marker(position, thing.name)
-
-
+      add_marker(position, thing.name, thing.imgpath, thing.url, null)
      })
 
     set_center()
 }
 
 
+function start_map() {
+  google.maps.event.addDomListener(window, 'load', initialize);
+}
 
 
-function add_marker(center, title) {
+function add_marker(center, title, imgpath, url, category) {
+    var content = '<a href="'+url+'" id="content">'+
+      '<h4 id="firstHeading" class="firstHeading">'+title+'</h4>'+
+      '<div id="bodyContent">'+
+      '<p><img style="width: 250px;" src="'+imgpath+'"></p>'+
+      '</div>'+
+      '</div>';
+    
+    var image = '/img/cat_food.png';
 
-  new google.maps.Marker({
-    map: map, 
-    position: center,
-    title: title
+    var infowindow = new google.maps.InfoWindow({
+      content: content,
+      maxWidth: 500
+    });
 
-  })
+    var marker = new google.maps.Marker({
+      position: center,
+      map: map,
+      title: title,
+      icon: image
+      });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
 
   // map.setCenter(center);
 
@@ -53,6 +70,3 @@ function set_center() {
 
 // })
 
-function start_map(){
-  google.maps.event.addDomListener(window, 'load', initialize);
-}
