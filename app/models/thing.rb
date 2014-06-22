496b1e55305c925027da83d7f57d062dc78e4706
gitfelix <felixmarkusweber@yahoo.it>
2014-06-22 03:10:19 +0200

    added bounty_mailer

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -6,7 +6,7 @@ class Thing < ActiveRecord::Base
     Bounty.where(name: name, found: false).each do |bounty|
 
       bounty.update_attribute(:found, true)
-      ActionMailer::Base.mail(:from => 'from@domain.com', :to => bounty.email,  :subject => "Your Item was Found!", :body => bounty.name).deliver
+      BountyMailer.bounty_found(bounty, self).deliver
 
     end
   end
